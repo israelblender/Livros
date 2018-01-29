2733be8f4f524b686b6a498a6b2f11b120a1f05f
israelblender <modelador.criar3d@gmail.com>
2018-01-29 00:14:41 -0300

    Idependendia das classes contidas em Table
    insercao de funcoes que controlam os binds de table, implementacao da margem de scroll ajustado a tabela dentro do widget Table, insercao do evento mouseWheel para rolagem com mouse, insercao das funcoes de evento para mudanca de linha atravez de botao ou teclas

--- src/Table.py
+++ src/Table.py
@@ -4,22 +4,68 @@ __date__ = "$30/12/2017 21:54:12$"
 
 import Tkinter as tk
 from collections import namedtuple
+            
+class Bind:
+    """Configurar funcao para bind necessitando de dois parametros na funcao de callback:\n\tdef funcao(events, unique_id)"""
+    dictFunctions = {}
+    def __init__(self, string, myFunction, type="only"): #type pode ser only ou all para ser acionado de qualquer widget
+            self.string = string
+            self.myFunction = myFunction
+            self.dictFunctions[string] = self._subFunction, type
 
-class Table(object, tk.Frame):
+    def _subFunction(self, events):
+            #unique_id = events.widget.unique_id
+            unique_id = Table.frameCelulas
+            #print events, unique_id
+            self.myFunction(events, unique_id)
+                
+class Binds:
+    """Retem todas as binds registradas na table"""
+    def __init__(self): pass
+    def _delBinds(self, widget):
+        for bindKeyStr in Bind.dictFunctions:
+            if not Bind.dictFunctions.get(bindKeyStr)[1] == "all":
+                widget.unbind_all(bindKeyStr)#, Bind.dictFunctions.get(bindString))
+    
+    def _addBinds(self, widget):
+        for bindKeyStr in Bind.dictFunctions:
+            widget.bind_all(bindKeyStr, Bind.dictFunctions.get(bindKeyStr)[0])
+
+class Table(object, tk.Frame, Binds):
+    _idBindMouse = None
+    frameCelulas = []
+    canvas = None
     def __new__(cls, *args, **kwargs):
-        return super(Table, cls).__new__(cls, *args, **kwargs)
-
+        if not hasattr(Table, "_instance"):
+                cls._instance = super(Table, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+    
+    def activeBind(self, event):
+        if hasattr(event.widget, "widgetName") and event.widget.widgetName == "table":
+            if not self._idBindMouse:
+                #self._idBindMouse = self.canvas.bind_all("<MouseWheel>", self.mouseWheelEvent)                
+                self._addBinds(widget = self.canvas)
+    def deactiveBind(self, event):
+        self._delBinds(widget = self.canvas)
+        #self.canvas.unbind_all("<MouseWheel>")
+        self._idBindMouse = None
+        
     def __init__(self, master, headers, **kwargs):
         tk.Frame.__init__(self, master, cnf=kwargs)
+        Binds.__init__(self)
+        #Bind.__init__(self)
+        Table.bind = Bind
         frameCanvasScrolly = tk.Frame(self)
         frameCanvasScrolly.pack(side=tk.TOP, fill="both", expand=True)
-        self.canvas = canvas = tk.Canvas(frameCanvasScrolly, scrollregion=(0, 0, 1100, 1000), background="#CAE1FF")
+        Table.canvas = self.canvas = canvas = tk.Canvas(master = frameCanvasScrolly, scrollregion=(0, 0, 1100, 1000), background="#CAE1FF")
         canvas.pack(side=tk.LEFT, fill="both", expand=True)
+        canvas.bind_all("<Motion>", self.activeBind)#acionado quando entra no widget table
+        canvas.bind("<Leave>", self.deactiveBind)#acionado quando sai do widget table
         
         self.frameCelulas = tk.Frame(canvas)
         canvas.create_window(0, 0, window=self.frameCelulas, anchor=tk.NW)
 
-        scrolly = tk.Scrollbar(frameCanvasScrolly, orient=tk.VERTICAL, command=canvas.yview)
+        self.scrolly = scrolly = tk.Scrollbar(frameCanvasScrolly, orient=tk.VERTICAL, command=canvas.yview)
         scrolly.pack(side=tk.LEFT, fill="y")
         #scrolly.grid(row=0, column=1)
         canvas['yscrollcommand'] = scrolly.set
@@ -38,18 +84,31 @@ class Table(object, tk.Frame):
         self.configColors(bg="#B4CDCD", bg_header="#68838B")
         self.configHeader(headers)
         self._unique_id_selected = None
-    def configSize(self, width, height):
-        self.config(width=width, height=height)#Muda tamanho do frame Table
-        #self.canvas.config(width=width, height=height)
-    
+        
     def configHeader(self, headers):
         self.headers = headers
         self.infoSizeCols = dict(headers)
-        self.dict_headers = {}
+        self.dict_idname_colname = {}
+        self.dict_idname_index = {}
         for index, elem in enumerate(headers):
-            self.dict_headers[index] = elem[0]
-        self.insertLine(-1, self.dict_headers, "header")
-
+            self.dict_idname_colname[index] = elem[0], elem[1][0]
+            self.dict_idname_index[elem[0]] = index
+        self.insertLine(-1, self.dict_idname_colname, "header")
+        
+    def mouseWheelEvent(self, event, unique_id):
+        if event.delta < 0: #Move para cima
+            self.canvas.yview_scroll(1, tk.UNITS)
+        elif event.delta > 0: #Move para baixo
+            self.canvas.yview_scroll(-1, tk.UNITS)
+    
+    def _updateRegion(self):#Atualiza a região para que o scroll se encaixe no quadro da tabela
+        self.frameCelulas.update()
+        self.canvas.config(scrollregion=(0, 0, self.frameCelulas.winfo_width(), self.frameCelulas.winfo_height()))#Atualiza scroll x e y para o tamanho da tabela
+        
+    def configSize(self, width, height):
+        self.config(width=width, height=height)#Muda tamanho do frame Table
+        #self.canvas.config(width=width, height=height)
+        
     def configColors(self, bg, bg_header):
         self._colorBgPatern = bg
         self._colorBgHeader = bg_header
@@ -58,83 +117,79 @@ class Table(object, tk.Frame):
         if len(elements) != len(self.infoSizeCols):
             raise Exception("Erro", u"A quantidade de elementos informados nao e igual a quantidade registrada!")
         return self.insertLine(unique_id, elements, "cel")
-
-    def insertLine(self, unique_id, elements, type):
-        self._insertCelulasByElement(unique_id, elements, type)
-        self.row += 1
     
-    def _updateRegion(self):#Atualiza a região para que o scroll se encaixe no quadro da tabela
-        self.frameCelulas.update()
-        self.canvas.config(scrollregion=(0, 0, self.frameCelulas.winfo_width(), self.frameCelulas.winfo_height()))
-        
-    def _insertCelulasByElement(self, unique_id, cols_values, type):
+    def insertLine(self, unique_id, elements, type):
         col = 0
+        
         if type == "cel":
-            for index in range(len(cols_values)):
-                col_key = self.dict_headers.get(index)
+            for index in range(len(elements)):
+                idname = self.dict_idname_colname.get(index)[0]
+
                 cel = self._insertCelula(row=self.row, col=col, 
                     unique_id = unique_id, 
-                    width = self.infoSizeCols.get(col_key),
-                    value = cols_values.get(col_key),
+                    width = self.infoSizeCols.get(idname)[1],
+                    value = elements.get(idname),
                     background=self._colorBgPatern)
-                self._addBinds(cel)
+                self._addBinds(widget=cel)
                 col += 1
         elif type == "header":
-            for index in range(len(cols_values)):
-                col_key = self.dict_headers.get(index)
+            for index in range(len(elements)):
+                idname = self.dict_idname_colname.get(index)[0]
+                
                 self._insertCelula(row=0, col=col, 
                         unique_id = unique_id, 
-                        width = self.infoSizeCols.get(col_key),
-                        value = self.dict_headers.get(index),
+                        width = self.infoSizeCols.get(idname)[1],
+                        value = self.dict_idname_colname.get(index)[1],
                         relief=tk.GROOVE,
                         background=self._colorBgHeader)
                 col += 1
-    
+        self.row += 1
+        
     #def _bindFunction(self, events):
         #print events.char, events.delta, events.height, events.keycode, events.keysym, events.keysym_num, events.num, events.send_event, events.serial, events.state, events.time, events.type, events.widget, events.width, events.x, events.x_root, events.y, events.y_root
-            
+    
     def _insertCelula(self, row, col, unique_id, width, value="", **cnf):
         cel = tk.Label(self.frameCelulas, width=width, text=value, cnf=cnf)
         cel.grid(row=row, column=col)
         cel.unique_id = unique_id
+        cel.widgetName = "table"
         return cel
-
-    def _addBinds(self, celula):
-        for bindKeyStr in self.bindLine.dictFunctions:
-            celula.bind(bindKeyStr, self.bindLine.dictFunctions.get(bindKeyStr))
-            
-    class bindLine:
-	dictFunctions = {}
-	def __init__(self, string, myFunction):
-		self.string = string
-		self.myFunction = myFunction
-		self.dictFunctions[string] = self._funcao
-                
-	def _funcao(self, events):
-		unique_id = events.widget.unique_id
-                #print events, unique_id
-		self.myFunction(events, unique_id)
         
-    def selectRow(self, events, unique_id):
+    def selectRowEvent(self, events, unique_id):
         self._unique_id_selected = unique_id#events.widget.unique_id
         widget = events.widget
-        self.selectRowByNumberLine(widget = widget, line = int(widget.grid_info().get("row")) )
-            
-    def selectRowByNumberLine(self, widget, line):
-        widgetsInLine = widget.grid_info().get("in").grid_slaves(row=line)
-        self._diselectLastWidgets()#Se nao for a primeira selecao, diselecione os ultimos widgets selecionados
-        self.selectWidgets(widgetsInLine)
+        self.selectRowByNumberLine(row = int(widget.grid_info().get("row")) )
+    
+    def selectPreviousRowEvent(self, events, unique_id=None):
+        """
+        Função para selecionar a linha antecessora acima acionada por um evento
+        """
+        self._unique_id_selected = unique_id
+        self.selectRowByNumberLine( row = self.rowCurrent - 1 )
         
+    def selectNextRowEvent(self, events, unique_id=None):
+        """
+        Função para selecionar a proxima linha abaixo acionada por um evento
+        """
+        self._unique_id_selected = unique_id
+        self.selectRowByNumberLine( row = self.rowCurrent + 1 )
+    
+    def selectRowByNumberLine(self, row):
+        #widgetsInLine = widget.grid_info().get("in").grid_slaves(row=row)
+        widgetsInLine = self.frameCelulas.grid_slaves(row=row)
+        self._diselectLastWidgetsBackgroundColors()#Se nao for a primeira selecao, diselecione os ultimos widgets selecionados
+        self.rowCurrent = row#self.getRowNumber(widgetsInLine)
+        self._selectCurrentWidgetsBackgroundColors(widgetsInLine)
     
     def getIdSelected(self):#Retorna o id retornado por selectRow
         return self._unique_id_selected
         
-    def selectWidgets(self, widgets):
+    def _selectCurrentWidgetsBackgroundColors(self, widgets):
         for widget in widgets:
             widget.config(background="#008B8B")
         self._lastWidgetsSelecteds = widgets
             
-    def _diselectLastWidgets(self):
+    def _diselectLastWidgetsBackgroundColors(self):
         if self._lastWidgetsSelecteds:
             for widget in self._lastWidgetsSelecteds:
                 widget.config(background=self._colorBgPatern)
@@ -177,8 +232,8 @@ if __name__ == "__main__":
     window.geometry("1050x550+250+100")
     table = Table(window, (("id", 8), ("nome", 40), ("idade",10)), background="green", relief=tk.RIDGE)
     
-    table.bindLine("<Button-1>", table.selectRow)
-    table.bindLine("<Button-3>", callWindowEdit)
+    table.Bind("<Button-1>", table.selectRow)
+    table.Bind("<Button-3>", callWindowEdit)
     
     table.insertRow(unique_id=5, elements={"id": "5", "nome": "Alguma pessoa", "idade":"18"})
     table.insertRow(unique_id=12, elements={"id": "12", "nome": "Fulano", "idade":"15"})
