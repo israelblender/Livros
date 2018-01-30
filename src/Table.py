8776adb47debc04bbb9db1ad098a0dabcb21f810
israelblender <modelador.criar3d@gmail.com>
2018-01-29 22:59:11 -0300

    Ajustes de eventos relacionados ao bind
    implementacao de controle de acoes no metodo oculto _subFunction da classe BindTable, Classe Bind renomeado para BindTable, Frame frameCelulas removido da instancia e movido para escopo da classe

--- src/Table.py
+++ src/Table.py
@@ -4,34 +4,47 @@ __date__ = "$30/12/2017 21:54:12$"
 
 import Tkinter as tk
 from collections import namedtuple
-            
-class Bind:
+
+class BindTable:
     """Configurar funcao para bind necessitando de dois parametros na funcao de callback:\n\tdef funcao(events, unique_id)"""
     dictFunctions = {}
-    def __init__(self, string, myFunction, type="only"): #type pode ser only ou all para ser acionado de qualquer widget
-            self.string = string
-            self.myFunction = myFunction
-            self.dictFunctions[string] = self._subFunction, type
-
+    def __init__(self, string, myFunction, type="only", action=""): #type pode ser only ou all para ser acionado de qualquer widget
+        self.string = string
+        self.myFunction = myFunction
+        self.dictFunctions[string] = self._subFunction, type, action
+        self.action = action
+        
     def _subFunction(self, events):
-            #unique_id = events.widget.unique_id
-            unique_id = Table.frameCelulas
-            #print events, unique_id
-            self.myFunction(events, unique_id)
+        unique_id = None
+        if not self.action: #Se action for nulo
+            unique_id = events.widget.unique_id
+        else: #Se action for up ou down
+            if self.action == "up":
+                unique_id_ = Table.frameCelulas.grid_slaves(row=Table.rowCurrent-1, column=0)[0].unique_id
+                unique_id = unique_id_
+            elif self.action == "down":
+                unique_id_ = Table.frameCelulas.grid_slaves(row=Table.rowCurrent+1, column=0)[0].unique_id
+                unique_id = unique_id_
+            
+        self.myFunction(events, unique_id)
                 
 class Binds:
     """Retem todas as binds registradas na table"""
     def __init__(self): pass
     def _delBinds(self, widget):
-        for bindKeyStr in Bind.dictFunctions:
-            if not Bind.dictFunctions.get(bindKeyStr)[1] == "all":
-                widget.unbind_all(bindKeyStr)#, Bind.dictFunctions.get(bindString))
+        for bindKeyStr in BindTable.dictFunctions:
+            if not BindTable.dictFunctions.get(bindKeyStr)[1] == "all":
+                widget.unbind_all(bindKeyStr)#, BindTable.dictFunctions.get(bindString))
     
     def _addBinds(self, widget):
-        for bindKeyStr in Bind.dictFunctions:
-            widget.bind_all(bindKeyStr, Bind.dictFunctions.get(bindKeyStr)[0])
-
+        for bindKeyStr in BindTable.dictFunctions:
+            widget.bind_all(bindKeyStr, BindTable.dictFunctions.get(bindKeyStr)[0])
+    
 class Table(object, tk.Frame, Binds):
+    """Widget para gerar tabela numa Gui
+    Funcoes principais:
+    
+    """
     _idBindMouse = None
     frameCelulas = []
     canvas = None
@@ -39,31 +52,23 @@ class Table(object, tk.Frame, Binds):
         if not hasattr(Table, "_instance"):
                 cls._instance = super(Table, cls).__new__(cls, *args, **kwargs)
         return cls._instance
-    
-    def activeBind(self, event):
-        if hasattr(event.widget, "widgetName") and event.widget.widgetName == "table":
-            if not self._idBindMouse:
-                #self._idBindMouse = self.canvas.bind_all("<MouseWheel>", self.mouseWheelEvent)                
-                self._addBinds(widget = self.canvas)
-    def deactiveBind(self, event):
-        self._delBinds(widget = self.canvas)
-        #self.canvas.unbind_all("<MouseWheel>")
-        self._idBindMouse = None
         
     def __init__(self, master, headers, **kwargs):
         tk.Frame.__init__(self, master, cnf=kwargs)
         Binds.__init__(self)
-        #Bind.__init__(self)
-        Table.bind = Bind
+        #BindTable.__init__(self)
+        Table.bind = BindTable
         frameCanvasScrolly = tk.Frame(self)
         frameCanvasScrolly.pack(side=tk.TOP, fill="both", expand=True)
         Table.canvas = self.canvas = canvas = tk.Canvas(master = frameCanvasScrolly, scrollregion=(0, 0, 1100, 1000), background="#CAE1FF")
+        
         canvas.pack(side=tk.LEFT, fill="both", expand=True)
         canvas.bind_all("<Motion>", self.activeBind)#acionado quando entra no widget table
         canvas.bind("<Leave>", self.deactiveBind)#acionado quando sai do widget table
         
-        self.frameCelulas = tk.Frame(canvas)
-        canvas.create_window(0, 0, window=self.frameCelulas, anchor=tk.NW)
+        Table.frameCelulas = tk.Frame(canvas)
+        #BindTable.reportWidgetEvent(Table.frameCelulas)
+        canvas.create_window(0, 0, window=Table.frameCelulas, anchor=tk.NW)
 
         self.scrolly = scrolly = tk.Scrollbar(frameCanvasScrolly, orient=tk.VERTICAL, command=canvas.yview)
         scrolly.pack(side=tk.LEFT, fill="y")
@@ -84,7 +89,7 @@ class Table(object, tk.Frame, Binds):
         self.configColors(bg="#B4CDCD", bg_header="#68838B")
         self.configHeader(headers)
         self._unique_id_selected = None
-        
+            
     def configHeader(self, headers):
         self.headers = headers
         self.infoSizeCols = dict(headers)
@@ -101,9 +106,9 @@ class Table(object, tk.Frame, Binds):
         elif event.delta > 0: #Move para baixo
             self.canvas.yview_scroll(-1, tk.UNITS)
     
-    def _updateRegion(self):#Atualiza a região para que o scroll se encaixe no quadro da tabela
-        self.frameCelulas.update()
-        self.canvas.config(scrollregion=(0, 0, self.frameCelulas.winfo_width(), self.frameCelulas.winfo_height()))#Atualiza scroll x e y para o tamanho da tabela
+    def updateRegion(self):#Atualiza a região para que o scroll se encaixe no quadro da tabela
+        Table.frameCelulas.update()
+        self.canvas.config(scrollregion=(0, 0, Table.frameCelulas.winfo_width(), Table.frameCelulas.winfo_height()))#Atualiza scroll x e y para o tamanho da tabela
         
     def configSize(self, width, height):
         self.config(width=width, height=height)#Muda tamanho do frame Table
@@ -144,12 +149,9 @@ class Table(object, tk.Frame, Binds):
                         background=self._colorBgHeader)
                 col += 1
         self.row += 1
-        
-    #def _bindFunction(self, events):
-        #print events.char, events.delta, events.height, events.keycode, events.keysym, events.keysym_num, events.num, events.send_event, events.serial, events.state, events.time, events.type, events.widget, events.width, events.x, events.x_root, events.y, events.y_root
     
     def _insertCelula(self, row, col, unique_id, width, value="", **cnf):
-        cel = tk.Label(self.frameCelulas, width=width, text=value, cnf=cnf)
+        cel = tk.Label(Table.frameCelulas, width=width, text=value, cnf=cnf)
         cel.grid(row=row, column=col)
         cel.unique_id = unique_id
         cel.widgetName = "table"
@@ -157,32 +159,38 @@ class Table(object, tk.Frame, Binds):
         
     def selectRowEvent(self, events, unique_id):
         self._unique_id_selected = unique_id#events.widget.unique_id
+        print "UNIQUEID: ", unique_id
         widget = events.widget
+        #self.rowSelected = widget.grid_info().get("row")
         self.selectRowByNumberLine(row = int(widget.grid_info().get("row")) )
     
     def selectPreviousRowEvent(self, events, unique_id=None):
-        """
-        Função para selecionar a linha antecessora acima acionada por um evento
-        """
+        """Função para selecionar a linha antecessora acima acionada por um evento"""
         self._unique_id_selected = unique_id
-        self.selectRowByNumberLine( row = self.rowCurrent - 1 )
+        print "UNIQUEID: ", unique_id
+        self.selectRowByNumberLine( row = Table.rowCurrent - 1 )
         
     def selectNextRowEvent(self, events, unique_id=None):
-        """
-        Função para selecionar a proxima linha abaixo acionada por um evento
-        """
+        """Função para selecionar a proxima linha abaixo acionada por um evento"""
         self._unique_id_selected = unique_id
-        self.selectRowByNumberLine( row = self.rowCurrent + 1 )
+        print "UNIQUEID: ", unique_id
+        self.selectRowByNumberLine( row = Table.rowCurrent + 1 )
     
     def selectRowByNumberLine(self, row):
+
         #widgetsInLine = widget.grid_info().get("in").grid_slaves(row=row)
-        widgetsInLine = self.frameCelulas.grid_slaves(row=row)
-        self._diselectLastWidgetsBackgroundColors()#Se nao for a primeira selecao, diselecione os ultimos widgets selecionados
-        self.rowCurrent = row#self.getRowNumber(widgetsInLine)
+        self._diselectLastWidgetsBackgroundColors() #Se nao for a primeira selecao, diselecione os ultimos widgets selecionados
+        Table.rowCurrent = row # Obtem a linha atual
+        widgetsInLine = Table.frameCelulas.grid_slaves(row=row)# Widgets presentes nessa linha
         self._selectCurrentWidgetsBackgroundColors(widgetsInLine)
+        self._unique_id_selected = widgetsInLine[0].unique_id
     
     def getIdSelected(self):#Retorna o id retornado por selectRow
         return self._unique_id_selected
+    def getRowCurrent(self):#Retorna a linha selecionada atualmente
+        try: 
+            return Table.rowCurrent
+        except: print "CALL selectRowByNumberLine(row=?) FUNCTION!"
         
     def _selectCurrentWidgetsBackgroundColors(self, widgets):
         for widget in widgets:
@@ -193,18 +201,30 @@ class Table(object, tk.Frame, Binds):
         if self._lastWidgetsSelecteds:
             for widget in self._lastWidgetsSelecteds:
                 widget.config(background=self._colorBgPatern)
+    
+    def activeBind(self, event):
+        if hasattr(event.widget, "widgetName") and event.widget.widgetName == "table":
+            if not self._idBindMouse:
+                #self._idBindMouse = self.canvas.bind_all("<MouseWheel>", self.mouseWheelEvent)                
+                self._addBinds(widget = self.canvas)
+    def deactiveBind(self, event):
+        self._delBinds(widget = self.canvas)
+        #self.canvas.unbind_all("<MouseWheel>")
+        self._idBindMouse = None
 
 if __name__ == "__main__":
     global datas_
     def action():
-        table.configHeader((("numero", 15), ("nome completo", 40), ("idade",10)))
+        table.configHeader((("number", "numero", 15), ("name", "nome completo", 40), ("old","old",10)))
     def actionChangeCanvasSize():
         table.configSize(100, 700)
 
-    def windowEdit(master, unique_id):
+    def windowEdit(master, position, unique_id):
         global nomeVar, idadeVar
         window = tk.Toplevel(master)
         window.resizable(False, False)
+        #print dir(window), window.winfo_screenwidth(), 
+        
         #window.attributes("-topmost", 1)
         window.transient(master)
         frame = tk.Frame(window)
@@ -216,52 +236,60 @@ if __name__ == "__main__":
         idadeVar = tk.StringVar()
         tk.Label(frame, text="Idade").grid(row=1, column=0)
         tk.Entry(frame, textvariable=idadeVar).grid(row=1, column=1)
+        
+        window.update()
+        window.geometry(str(window.winfo_width())+"x"+str(window.winfo_height())+"+"+str(position[0])+"+"+str(position[1]))
         setDatas(unique_id)
         
     def setDatas(unique_id):
-        data = datas_.get(str(unique_id))        
-        nomeVar.set(data.get("nome"))
-        idadeVar.set(data.get("idade"))
         
-    def callWindowEdit(events, unique_id):
-        #print "callWindowEdit Chamado"
-        windowEdit(window, table.getIdSelected())
+        data = datas_.get(str(unique_id))    
+        nomeVar.set(data.get("name"))
+        idadeVar.set(data.get("old"))
+        
+    def callWindowEditEvent(events, unique_id):
+        #print dir(events)
+        print events.x, events.y, events.x_root, events.y_root
+        windowEdit(window, (events.x_root, events.y_root), table.getIdSelected())
         
     window = tk.Tk()
     window.title("Tabela")
     window.geometry("1050x550+250+100")
-    table = Table(window, (("id", 8), ("nome", 40), ("idade",10)), background="green", relief=tk.RIDGE)
+    table = Table(window, (("id", ("id", 8)), ("name", ("nome", 40)), ("old", ("idade",10))), background="green", relief=tk.RIDGE)
     
-    table.Bind("<Button-1>", table.selectRow)
-    table.Bind("<Button-3>", callWindowEdit)
+    table.bind("<Button-1>", table.selectRowEvent)
+    table.bind("<Button-3>", callWindowEditEvent)
     
-    table.insertRow(unique_id=5, elements={"id": "5", "nome": "Alguma pessoa", "idade":"18"})
-    table.insertRow(unique_id=12, elements={"id": "12", "nome": "Fulano", "idade":"15"})
-    table.insertRow(unique_id=14, elements={"id": "14", "nome": "Ciclano", "idade":"22"})
-    table.insertRow(unique_id=18, elements={"id": "18", "nome": "Beltrano", "idade":"26"})
-    table.insertRow(unique_id=18, elements={"id": "18", "nome": "Beltrano", "idade":"26"})
-    table.insertRow(unique_id=18, elements={"id": "18", "nome": "Beltrano", "idade":"26"})
-    table.insertRow(unique_id=18, elements={"id": "18", "nome": "Beltrano", "idade":"26"})
-    table.insertRow(unique_id=18, elements={"id": "18", "nome": "Beltrano", "idade":"26"})
-    table.insertRow(unique_id=18, elements={"id": "18", "nome": "Beltrano", "idade":"26"})
-    table.insertRow(unique_id=18, elements={"id": "18", "nome": "Beltrano", "idade":"26"})
-    table.insertRow(unique_id=18, elements={"id": "18", "nome": "Beltrano", "idade":"26"})
-    table.insertRow(unique_id=18, elements={"id": "18", "nome": "Beltrano", "idade":"26"})
-    table.insertRow(unique_id=18, elements={"id": "18", "nome": "Beltrano", "idade":"26"})
-    table.insertRow(unique_id=18, elements={"id": "18", "nome": "Beltrano", "idade":"26"})
-    table.insertRow(unique_id=18, elements={"id": "18", "nome": "Beltrano", "idade":"26"})
-    table.insertRow(unique_id=18, elements={"id": "18", "nome": "Beltrano", "idade":"26"})
-    table.insertRow(unique_id=18, elements={"id": "18", "nome": "Beltrano", "idade":"26"})
-    table.insertRow(unique_id=18, elements={"id": "18", "nome": "Beltrano", "idade":"26"})
-    table.insertRow(unique_id=18, elements={"id": "18", "nome": "Beltrano", "idade":"26"})
-    table.insertRow(unique_id=18, elements={"id": "18", "nome": "Beltrano", "idade":"26"})
-    table.insertRow(unique_id=18, elements={"id": "18", "nome": "Beltrano", "idade":"26"})
-    
+    table.insertRow(unique_id=5, elements={"id": "5", "name": "Alguma pessoa", "old":"18"})
+    table.insertRow(unique_id=12, elements={"id": "12","name": "Fulano","old":"15"})
+    table.insertRow(unique_id=14, elements={"id": "14","name": "Ciclano","old":"22"})
+    table.insertRow(unique_id=18, elements={"id": "18","name": "Beltrano","old":"26"})
+    table.insertRow(unique_id=18, elements={"id": "18","name": "Beltrano","old":"26"})
+    table.insertRow(unique_id=18, elements={"id": "18","name": "Beltrano","old":"26"})
+    table.insertRow(unique_id=18, elements={"id": "18","name": "Beltrano","old":"26"})
+    table.insertRow(unique_id=18, elements={"id": "18","name": "Beltrano","old":"26"})
+    table.insertRow(unique_id=18, elements={"id": "18","name": "Beltrano","old":"26"})
+    table.insertRow(unique_id=18, elements={"id": "18","name": "Beltrano","old":"26"})
+    table.insertRow(unique_id=18, elements={"id": "18","name": "Beltrano","old":"26"})
+    table.insertRow(unique_id=18, elements={"id": "18","name": "Beltrano","old":"26"})
+    table.insertRow(unique_id=18, elements={"id": "18","name": "Beltrano","old":"26"})
+    table.insertRow(unique_id=18, elements={"id": "18","name": "Beltrano","old":"26"})
+    table.insertRow(unique_id=18, elements={"id": "18","name": "Beltrano","old":"26"})
+    table.insertRow(unique_id=18, elements={"id": "18","name": "Beltrano","old":"26"})
+    table.insertRow(unique_id=18, elements={"id": "18","name": "Beltrano","old":"26"})
+    table.insertRow(unique_id=18, elements={"id": "18","name": "Beltrano","old":"26"})
+    table.insertRow(unique_id=18, elements={"id": "18","name": "Beltrano","old":"26"})
+    table.insertRow(unique_id=18, elements={"id": "18","name": "Beltrano","old":"26"})
+    table.insertRow(unique_id=18, elements={"id": "18","name": "Beltrano","old":"26"})
+    table.updateRegion()
+    #table.bind("<Up>", table.selectPreviousRowEvent)
+    table.bind("<Up>", table.selectPreviousRowEvent, "only", action="up")
+    table.bind("<Down>", table.selectNextRowEvent, "only", action="down")
 
-    datas_ ={"5":{"nome": "Alguma pessoa", "idade":"18"},
-    "12":{"nome": "Fulano", "idade":"15"},
-    "14":{"nome": "Ciclano", "idade":"22"},
-    "18":{"nome": "Beltrano", "idade":"26"}}
+    datas_ ={"5":{"name": "Alguma pessoa","old":"18"},
+    "12":{"name": "Fulano","old":"15"},
+    "14":{"name": "Ciclano","old":"22"},
+    "18":{"name": "Beltrano","old":"26"}}
         
     table.pack(fill="both", expand=True, padx=10, pady=10)
 
